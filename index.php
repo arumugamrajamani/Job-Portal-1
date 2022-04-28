6042548671e58ec7b8260f158ade03fdd74cdf9e
Justine Rome Guillermo <58583590+jastineee@users.noreply.github.com>
2022-04-28 11:35:51 +0800

    removed meta tags

--- index.php
+++ index.php
@@ -2,17 +2,11 @@
 <html lang="en">
     <head>
         <meta charset="utf-8">
-        <meta http-equiv="pragma" content="no-cache" />
-        <meta http-equiv="cache-control" content="max-age=604800" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-        <meta name="GROUP:" content="EXCLUSIVE">
-
         <title>Job Portal</title>
-
-
-
     </head>
+
+
+    
     <body>
         <h1>Try ko mag re sync</h1>
     </body>

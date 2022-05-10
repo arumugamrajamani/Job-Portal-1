c8d873e3def61b96ec779124f520f05679ba4019
Marc John <castillomarcjohn@gmail.com>
2022-05-10 15:59:26 +0800

    Fixed the merge conflict in head tag

--- jobseekersignup.php
+++ jobseekersignup.php
@@ -14,13 +14,6 @@
     <!-- Toast CDN for design of toastr -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js" integrity="sha512-VEd+nq25CkR676O+pLBnDW09R7VQX9Mdiij052gVCp5yVH3jGtH70Ho/UUv4mJDsEdTvqRCFZg0NKGiojGnUCw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <link rel="stylesheet" href="css/jobseekersignup.css">
-<<<<<<< HEAD
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
-    <?php
-    include 'js/jobseekerssignupAJAX.php';
-    ?>
-=======
->>>>>>> 3de2fec997119a915314e1214339b7a3dc5e00cc
 </head>
 
 <body>

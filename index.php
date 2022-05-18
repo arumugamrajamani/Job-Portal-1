4101ceebda3d3ca7aefaa69f081d7b57dd526d47
Marc John <castillomarcjohn@gmail.com>
2022-05-18 10:02:42 +0800

    Additional revision

--- index.php
+++ index.php
@@ -23,8 +23,6 @@
 
                 <div class="collapse navbar-collapse" id="toggleMobileMenu">
                     <ul class="navbar-nav ms-auto text-center">
-
-
                         <nav class="navbar navbar-light ">
                             <li>
                                 <a class="nav-link me-5 fs-5" href="#aboutus">About Us</a></li>

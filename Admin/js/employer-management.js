a1bb656225c8f3d0bb35b3b0a361bb48bdb80d1c
Marc John <castillomarcjohn@gmail.com>
2022-05-24 14:27:04 +0800

    Added search functionality to employers management

--- Admin/js/employer-management.js
+++ Admin/js/employer-management.js
@@ -2,12 +2,13 @@ $(document).ready(function() {
     // Call this function to reload the table data at first time
     load_data();
     // Function for loading of table data
-    function load_data(){
+    function load_data(search){
         $.ajax({
             url: "php/employer-management.inc.php",
             type: "POST",
             data: {
-                loadData: true
+                loadData: true,
+                search: search
             },
             success: function(data){
                 $('#body-h').html(data);
@@ -20,4 +21,14 @@ $(document).ready(function() {
         let src = $(this).find('img').attr('src')
         $('#view-logo').attr('src', src)
     })
+
+    // Trigger this when user started to search in the search bar
+    $('#search').keyup(function () {
+        let search = $(this).val();
+        if(search != ''){
+            load_data(search);
+        } else {
+            load_data();
+        }
+    });
 })

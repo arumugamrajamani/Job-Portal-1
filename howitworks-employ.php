2956da51535e6fb1fcecb7fb47b532566043b1e8
Marc John <castillomarcjohn@gmail.com>
2022-05-30 10:32:00 +0800

    Added how it works pages for employer and jobseeker

---
+++ howitworks-employ.php
@@ -0,0 +1,161 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>How it Works</title>
+    <!--Font-->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/howitworks-employ.css">
+    
+</head>
+
+<body>
+    <div class="color-overlay">
+        <div class="container-fluid">
+            <nav class="navbar navbar-expand-lg  h6 navbar-light bg fixed-top mx-0">
+                    <a href="index.html" class="navbar-brand ms-3">
+                    <img src="image/flogo.png" alt="Job Portal Logo" width="120" height="95"></a>
+
+                <div class="collapse navbar-collapse" id="toggleMobileMenu">
+                    <ul class="navbar-nav ms-auto text-center">
+
+
+                        <nav class="navbar navbar-light fw-bold">
+                            <li>
+                                <a class="nav-link me-5 fs-5" href="index.html#home">Home</a></li>
+                                <a class="nav-link me-5 fs-5" href="index.html#aboutus">About Us</a></li>
+                                <a class="nav-link me-5 fs-5" href="index.html#contactus">Contact Us</a></li>            
+                                <a class="nav-link me-5 fs-5" href="index.html#faq">FAQ</a></li>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle fs-5 me-4" href="#"
+                                id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">Signup</a>
+                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <li><a class="dropdown-item me-4 fs-5" href="companyregister.html">Employer</a></li>
+ 								<li><a class="dropdown-item me-4 fs-5"href="jobseekersignup.html">JobSeeker</a></li>
+                            </ul>
+
+                                <li><a class="nav-link fs-5 ms-3 me-5" href="login.html">Login</a></li>   
+                        </nav>
+                    </ul>
+                </div>
+            </nav>         
+    	</div>
+    </div><br><br><br><br><br><br>
+    <div class="container mb-5 shadow" style="background-color: #FDF6EC;height:100vh;border-radius: 20px;"><br>
+        <h3 class="fw-bold mt-5 text-center">How it Works</h3>
+        <div class="d-block ms-5">
+            <div class="d-flex ms-5">
+                <img src="image/step1.png" alt="" class="mt-5 mb-3" style="margin-left:200px;width: 150px; height: 150px;">
+                <div class="block ms-5 mt-5">
+                    <h4 class="fw-bold">Step 1</h4>
+                    <p> Post a Job or Search Resumes</p>
+                    <h4 class="fw-bold">Search Resumes</h4>
+                    <p> Proactively look for staff with the skills you need</p>
+                    <span class="input-icons"><i class="bi bi-search pt-1 ms-2" style="font-size: 20px;" ></i>
+                        <input class="form-control me-2 ps-5" style="background-color: #ECECEC; width: 430px;border: 0;" type="search" placeholder="Search" aria-label="Search">
+                    </span>
+                </div>
+            </div>
+            <div class="d-flex ms-5">
+                <img src="image/step2.png" alt="" class="mt-5 mb-3" style="margin-left:180px;width: 150px;">
+                <div class="block mt-5" style="margin-left: 67px;">
+                    <h4 class="fw-bold" id="step2">Step 2</h4>
+                    <div class="d-flex">
+                        <img src="image/check.png" alt="" style="width:35px; height:35px;">
+                        <p class="mt-2"> Find the perfect staff member</p>
+                    </div>
+                    <div class="d-flex">
+                        <img src="image/check.png" alt="" style="width:35px; height:35px;">
+                        <p class="">Communicate with them via simple email</p>
+                    </div>
+                    <div class="d-flex">
+                        <img src="image/check.png" alt="" style="width:35px; height:35px;">
+                        <p class="">Interview them like you would for a normal staff member</p>
+                    </div>
+                    <div class="d-flex">
+                        <img src="image/check.png" alt="" style="width:35px; height:35px;">
+                        <p class=""> Find the perfect fit.</p>
+                    </div>
+                </div>
+
+            </div>
+            
+            <div class="d-flex ms-5">
+                <img src="image/step3.png" alt="" class="mt-5 mb-3" style="margin-left:180px;width: 150px;">
+                <div class="block mt-5" style="margin-left: 67px;">
+                    <h4 class="fw-bold" id="step2">Step 3</h4>
+                    <div class="d-flex">
+                        <img src="image/check.png" alt="" style="width:35px; height:35px;">
+                        <p class="mt-2"> Find the perfect staff member</p>
+                    </div>
+                </div>
+            </div><br><br>
+            <p class="text-center">Got more questions? visit our <a href="index.html#faq" style="text-decoration: none;">FAQ</a> page!</p>
+        </div>
+
+    </div>
+
+    <footer class="page-footer shadow"><br>
+        <div class="container-fluid text-start text-md-left mt-5">
+            <div class="row">
+				<div class="col-md-2 mx-auto mb-4">
+                    <h6 class="text-uppercase fw-bold">JOB SEEKER</h6>
+                  
+                    <ul class="list-unstyled">
+                        <li class="my-0"><a href="#" class="text-dark" style="text-decoration: none;">Register</a></li>
+                        <li class="my-0"><a href="#" class="text-dark" style="text-decoration: none;">Job Search</a></li>
+                        <li class="my-0"><a href="howitworks-jobseeker.html" class="text-dark" style="text-decoration: none;">How it works</a></li>
+                    </ul>
+                </div>
+
+                <div class="col-md-2 mx-auto mb-4">
+                    <h6 class="text-uppercase fw-bold">EMPLOYER</h6>
+                    
+
+                    <ul class="list-unstyled">
+                        <li class="my-0"><a href="#" class="text-dark" style="text-decoration: none;">Register</a></li>
+                        <li class="my-0"><a href="#" class="text-dark" style="text-decoration: none;">Post a job</a></li>
+                        <li class="my-0"><a href="howitworks-employ.html" class="text-dark" style="text-decoration: none;">How it works</a></li>
+
+                    </ul>
+                </div>
+
+                <div class="col-md-2 mx-auto mb-4">
+                    <h6 class="text-uppercase fw-bold">OTHER INFORMATIONS</h6>
+                    
+                    <ul class="list-unstyled">
+                        <li class="my-0"></i> <a href="#aboutus" class="text-dark" style="text-decoration: none;">About us</a></li>
+                        <li class="my-0"></i> <a href="#faq" class="text-dark" style="text-decoration: none;">FAQ</a></li>
+                        <li class="my-0"></i> <a href="termsandcondition.html" class="text-dark" style="text-decoration: none;">Terms and conditions</a></li>
+                        <li class="my-0"></i><a href="privacypolicy.html" class="text-dark" style="text-decoration: none;">Privacy Policy</a></li>
+                        <li class="my-0"></i> <a href="termsofuse.html" class="text-dark" style="text-decoration: none;">Terms of use</a></li>
+                    </ul>
+                </div>
+                <div class="col-md-2 mx-auto mb-4 text-center">
+                    <h6 class="text-uppercase fw-bold">CONTACT US</h6>
+                    
+                    <ul class="list-unstyled d-flex">
+                        <img class="my-0" src="image/gmail.png" alt="logo" href="#" style="width: 50px; height: 25px">
+                        concerns.techploymentph@gmail.com
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <div class="container-fluid bg-white text-center">
+       
+        <h5 class="pt-2 ms-2"> Copyrignt © TechPloyment 2022. All Rights Reserved. Developed by MCC Interns 2022, Job Portal</h5>
+        
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ howitworks-jobseeker.php
@@ -0,0 +1,147 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>How it Works</title>
+     <!--Font-->
+     <link rel="preconnect" href="https://fonts.googleapis.com">
+     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@700&display=swap" rel="stylesheet">
+     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200&display=swap" rel="stylesheet">
+     
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/howitworks-jobseeker.css">
+    
+</head>
+
+<body>
+    <div class="color-overlay">
+        <div class="container-fluid">
+            <nav class="navbar navbar-expand-lg  h6 navbar-light bg fixed-top mx-0">
+                    <a href="index.html" class="navbar-brand ms-3">
+                    <img src="image/flogo.png" alt="Job Portal Logo" width="120" height="95"></a>
+
+                <div class="collapse navbar-collapse" id="toggleMobileMenu">
+                    <ul class="navbar-nav ms-auto text-center">
+
+
+                        <nav class="navbar navbar-light fw-bold">
+                            <li>
+                                <a class="nav-link me-5 fs-5" href="index.html#home">Home</a></li>
+                                <a class="nav-link me-5 fs-5" href="index.html#aboutus">About Us</a></li>
+                                <a class="nav-link me-5 fs-5" href="index.html#contactus">Contact Us</a></li>            
+                                <a class="nav-link me-5 fs-5" href="index.html#faq">FAQ</a></li>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle fs-5 me-4" href="#"
+                                id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">Signup</a>
+                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <li><a class="dropdown-item me-4 fs-5" href="companyregister.html">Employer</a></li>
+ 								<li><a class="dropdown-item me-4 fs-5"href="jobseekersignup.html">JobSeeker</a></li>
+                            </ul>
+
+                                <li><a class="nav-link fs-5 ms-3 me-5" href="login.html">Login</a></li>   
+                        </nav>
+                    </ul>
+                </div>
+            </nav>         
+    	</div>
+    </div><br><br><br><br><br><br>
+    <div class="container mb-5 shadow" style="background-color: #FDF6EC;height:100vh;border-radius: 20px;"><br>
+        <h3 class="fw-bold mt-5 text-center">How it Works</h3><br>
+        <section class="ms-5" style="background-color: rgba(0, 194, 214, 0.33);box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);height:150px;width: 93%;">
+            <p style="font-size: 22px;" class="text-center p-4 px-5">With today's technology, it is super easy to be hired by companies and 
+                then do the job from the comfort of your own home with your family. At
+                Tech-Ployment we'll help you explore online job opportunities and earn
+                 a living. Here are the 5 easy steps in applying your dream job!</p>
+        </section>
+        <div class="d-flex ms-5 mt-5">
+            <div class="block ms-5">
+                <div class="ms-4">
+                    <img src="image/job.png" alt="" style="width:280px;height:180px;">
+                
+                </div>
+                <div class="ms-4 mt-5">
+                    <img src="image/men.png" alt="" style="width:200px;height:200px;">
+                
+                </div>
+            </div>
+            <div class="block">
+                <h4 class="fw-bold">STEP 1</h4>
+                <p> Once you have an account, click the job board button</p>
+                <h4 class="fw-bold">STEP 2</h4>
+                <p> On the side panel, you can customize the type of job that you are looking</p>
+                <h4 class="fw-bold">STEP 3</h4>
+                <p> If you found the perfect job, click apply now!</p>
+                <h4 class="fw-bold">STEP 4</h4>
+                <p> 
+                    Supply the require informations such as your contact number, 
+                    resume or email
+                </p>
+                <h4 class="fw-bold">STEP 5</h4>
+                <p>  Wait for the reply of the employer and be ready to be hired!</p>
+                
+            </div> 
+        </div><br><br><br>
+        <p class="text-center">Got more questions? visit our <a href="index.html#faq" style="text-decoration: none;">FAQ</a> page!</p>
+
+    </div>
+
+    <footer class="page-footer shadow"><br>
+        <div class="container-fluid text-start text-md-left mt-5">
+            <div class="row">
+				<div class="col-md-2 mx-auto mb-4">
+                    <h6 class="text-uppercase fw-bold">JOB SEEKER</h6>
+                  
+                    <ul class="list-unstyled">
+                        <li class="my-0"><a href="#" class="text-dark" style="text-decoration: none;">Register</a></li>
+                        <li class="my-0"><a href="#" class="text-dark" style="text-decoration: none;">Job Search</a></li>
+                        <li class="my-0"><a href="#" class="text-dark" style="text-decoration: none;">How it works</a></li>
+                    </ul>
+                </div>
+
+                <div class="col-md-2 mx-auto mb-4">
+                    <h6 class="text-uppercase fw-bold">EMPLOYER</h6>
+                    
+
+                    <ul class="list-unstyled">
+                        <li class="my-0"><a href="#" class="text-dark" style="text-decoration: none;">Register</a></li>
+                        <li class="my-0"><a href="#" class="text-dark" style="text-decoration: none;">Post a job</a></li>
+                        <li class="my-0"><a href="howitworks-employ.html" class="text-dark" style="text-decoration: none;">How it works</a></li>
+
+                    </ul>
+                </div>
+
+                <div class="col-md-2 mx-auto mb-4">
+                    <h6 class="text-uppercase fw-bold">OTHER INFORMATIONS</h6>
+                    
+                    <ul class="list-unstyled">
+                        <li class="my-0"></i> <a href="#aboutus" class="text-dark" style="text-decoration: none;">About us</a></li>
+                        <li class="my-0"></i> <a href="#faq" class="text-dark" style="text-decoration: none;">FAQ</a></li>
+                        <li class="my-0"></i> <a href="termsandcondition.html" class="text-dark" style="text-decoration: none;">Terms and conditions</a></li>
+                        <li class="my-0"></i><a href="privacypolicy.html" class="text-dark" style="text-decoration: none;">Privacy Policy</a></li>
+                        <li class="my-0"></i> <a href="termsofuse.html" class="text-dark" style="text-decoration: none;">Terms of use</a></li>
+                    </ul>
+                </div>
+                <div class="col-md-2 mx-auto mb-4 text-center">
+                    <h6 class="text-uppercase fw-bold">CONTACT US</h6>
+                    
+                    <ul class="list-unstyled d-flex">
+                        <img class="my-0" src="image/gmail.png" alt="logo" href="#" style="width: 50px; height: 25px">
+                        concerns.techploymentph@gmail.com
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <div class="container-fluid bg-white text-center">
+       
+        <h5 class="pt-2 ms-2"> Copyrignt © TechPloyment 2022. All Rights Reserved. Developed by MCC Interns 2022, Job Portal</h5>
+        
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</body>
+</html>

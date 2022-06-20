9309efe1aea591a81976ec854d1a8c14e0d3a1a5
jastineee <201810083@fit.edu.ph>
2022-06-20 10:02:49 +0800

    added pre loader

--- Admin/recycle-bin-employer.php
+++ Admin/recycle-bin-employer.php
@@ -10,7 +10,8 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css"> <!-- jQuery cdn link below -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
+    <!-- jQuery cdn link below -->
     <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
     <!-- Toast CDN for functionality of toastr -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.css" integrity="sha512-3pIirOrwegjM6erE5gPSwkUzO+3cTjpnV9lexlNZqvupR64iZBnOOTiiLPb9M36zpMScbmUNIcHUqKD47M719g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
--- Admin/recycle-bin-jobpost.php
+++ Admin/recycle-bin-jobpost.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html lang="en">
+
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -11,13 +12,24 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
     <link rel="stylesheet" href="css/recycle-bin-jobpost.css">
+    <!-- jQuery cdn link below -->
+    <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+    <!-- Toast CDN for functionality of toastr -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.css" integrity="sha512-3pIirOrwegjM6erE5gPSwkUzO+3cTjpnV9lexlNZqvupR64iZBnOOTiiLPb9M36zpMScbmUNIcHUqKD47M719g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- Toast CDN for design of toastr -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js" integrity="sha512-VEd+nq25CkR676O+pLBnDW09R7VQX9Mdiij052gVCp5yVH3jGtH70Ho/UUv4mJDsEdTvqRCFZg0NKGiojGnUCw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <link rel="stylesheet" href="../css/preloader.css">
+
     <title>Recycle Bin Job Post</title>
 </head>
+
 <body>
+
+    <?php include_once '../include/preloader-display.php'; ?>
     <div class="color-overlay">
         <nav class="navbar navbar-expand-lg  h6 navbar-light bg fixed-top mx-0 shadow-sm">
             <a href="#" class="navbar-brand ms-5">
-            <img src="image/flogo.png" alt="Job Portal Logo" width="80" height="60"></a>
+                <img src="image/flogo.png" alt="Job Portal Logo" width="80" height="60"></a>
             <h6 class="position-relative">Recycle Bin Management</h6>
             <div class="dropdown ms-auto d-flex">
                 <h6 class="mt-2 fw-bold" id="adminFullName"></h6>
@@ -42,33 +54,33 @@
             </div>
         </nav>
     </div>
-<button class="openbtn" title="Open Sidebar" onclick="openNav()"><i id="list-i" class="bi bi-list fa-2x"></i></button>
-<div class="sidebar shadow-lg" id="mySidebar" class="sidebar">
-    <div class="menu">
-        <div class="item"><a href="javascript:void(0)" class="closebtn text-dark" title="Close Sidebar" onclick="closeNav()">×</a></div>
-        <div class="item"><a href="dashboard.php"><img class="me-3" src="image/dashboard.png"> Dashboard</a><br></div>
-        <div class="item"><a href="employer-management.php"><img class="me-3" src="image/employers.png"> Employers Management</a><br></div>
-        <div class="item"><a href="jobseeker-management.php"><img class="me-3" src="image/jobseeker.png"> Job Seeker Management</a><br></div>
-        <div class="item"><a href="jobpost-management.php"><img class="me-3" src="image/jobpost.png"> Job Post Management</a><br></div>
-        <div class="item"><a href="jobcategories-management.php"><img class="me-3" src="image/jobcategory.png"> Job Categories Management</a><br></div>
-        <div class="item">
-            <a class="sub-btn"><img class="me-3" src="image/profilesetting.png">Setting <i class="fa fa-angle-right dropdown"></i></a>
-            <div class="sub-menu">
-                <a href="system-settings.php" class="sub-item">System Settings</a>
-                <a href="aboutus-settings.php" class="sub-item">About Us Settings</a>
-                <a href="faq-settings.php" class="sub-item">Faq Settings</a>
-            </div>
-        </div><br>
-        <div class="item">
-            <a class="sub-btn"><img class="me-3" src="image/recycle-bin.png"> Recycle Bin <i class="fa fa-angle-right dropdown"></i></a>
-            <div class="sub-menu sub-display">
-                <a href="recycle-bin-employer.php" class="sub-item">Employers Management</a>
-                <a href="recycle-bin-jobseeker.php" class="sub-item">Job Seeker Management</a>
-                <a href="recycle-bin-jobpost.php" class="sub-item highlight">Job Post Management</a>
+    <button class="openbtn" title="Open Sidebar" onclick="openNav()"><i id="list-i" class="bi bi-list fa-2x"></i></button>
+    <div class="sidebar shadow-lg" id="mySidebar" class="sidebar">
+        <div class="menu">
+            <div class="item"><a href="javascript:void(0)" class="closebtn text-dark" title="Close Sidebar" onclick="closeNav()">×</a></div>
+            <div class="item"><a href="dashboard.php"><img class="me-3" src="image/dashboard.png"> Dashboard</a><br></div>
+            <div class="item"><a href="employer-management.php"><img class="me-3" src="image/employers.png"> Employers Management</a><br></div>
+            <div class="item"><a href="jobseeker-management.php"><img class="me-3" src="image/jobseeker.png"> Job Seeker Management</a><br></div>
+            <div class="item"><a href="jobpost-management.php"><img class="me-3" src="image/jobpost.png"> Job Post Management</a><br></div>
+            <div class="item"><a href="jobcategories-management.php"><img class="me-3" src="image/jobcategory.png"> Job Categories Management</a><br></div>
+            <div class="item">
+                <a class="sub-btn"><img class="me-3" src="image/profilesetting.png">Setting <i class="fa fa-angle-right dropdown"></i></a>
+                <div class="sub-menu">
+                    <a href="system-settings.php" class="sub-item">System Settings</a>
+                    <a href="aboutus-settings.php" class="sub-item">About Us Settings</a>
+                    <a href="faq-settings.php" class="sub-item">Faq Settings</a>
+                </div>
+            </div><br>
+            <div class="item">
+                <a class="sub-btn"><img class="me-3" src="image/recycle-bin.png"> Recycle Bin <i class="fa fa-angle-right dropdown"></i></a>
+                <div class="sub-menu sub-display">
+                    <a href="recycle-bin-employer.php" class="sub-item">Employers Management</a>
+                    <a href="recycle-bin-jobseeker.php" class="sub-item">Job Seeker Management</a>
+                    <a href="recycle-bin-jobpost.php" class="sub-item highlight">Job Post Management</a>
+                </div>
             </div>
         </div>
     </div>
-</div>
     <br><br><br>
     <div class="container-responsive p-md-5 mt-4 bg-white" id="main">
         <div class="d-flex justify-content-end">
@@ -94,7 +106,7 @@
                             <tbody class="tbody bg-light text-dark" id="body-h">
                                 <tr class="tr">
                                     <td data-title="Employer"><b><i class="bi bi-person-circle"></i> Full Name</b></td>
-                                    <td data-title="Company"><b>Company</b></td>  
+                                    <td data-title="Company"><b>Company</b></td>
                                     <td data-title="Job Category"><b>Web Development</b></td>
                                     <td data-title="Date Applied"><b>05/02/2022</b></td>
                                     <td data-title="Action">
@@ -197,7 +209,7 @@
                     </div>
                 </div>
             </section>
-        </div> 
+        </div>
         <nav aria-label="Page navigation example">
             <div class="entries">
                 </span>Show 1 to 3 of 3 entries</span>
@@ -209,7 +221,7 @@
                 <li class="page-item"><a class="page-link text-dark" href="#">3</a></li>
                 <li class="page-item"><a class="page-link bg-info text-dark" href="#">Next</a></li>
             </ul>
-        </nav> 
+        </nav>
     </div>
     <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog">
@@ -228,16 +240,17 @@
 
     <script>
         function openNav() {
-        document.getElementById("mySidebar").style.left = "0";
-        document.getElementById("main").style.marginLeft = "390px";
+            document.getElementById("mySidebar").style.left = "0";
+            document.getElementById("main").style.marginLeft = "390px";
         }
-        
+
         function closeNav() {
-        document.getElementById("mySidebar").style.left = "-100%";
-        document.getElementById("main").style.marginLeft= "230px";
+            document.getElementById("mySidebar").style.left = "-100%";
+            document.getElementById("main").style.marginLeft = "230px";
         }
     </script>
     <script src="js/navbar.js"></script>
     <script src="../js/preloader.js"></script>
 </body>
+
 </html>

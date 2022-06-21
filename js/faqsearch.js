20272b903d9cf30fb26d099de321d011aa887f7e
jastineee <201810083@fit.edu.ph>
2022-06-21 11:34:44 +0800

    faq search highlight working.

--- js/faqsearch.js
+++ js/faqsearch.js
@@ -1,32 +1,32 @@
-$(document).ready(function() {
+$(document).ready(function () {
     // Trigger this when user click search in FAQ section
-    $('#faq').on('click', '#faqsearch', function(){
+    $('#faq').on('click', '#faqsearch', function () {
         // Get the value of the search question
-        let searchText = $('#faqsearch-text').val();    
+        let searchText = $('#faqsearch-text').val();
         // Check if the search question is empty
-        if($.trim(searchText) != ""){
+        if ($.trim(searchText) != "") {
             searchHighlight(searchText);
-        } 
+        }
     })
 
     // Function for highlight search text
-    function searchHighlight(searchText){  
+    function searchHighlight(searchText) {
         $(".highlight").contents().unwrap(); // removes on span with the class highlight
-        $('.collapsed').each(function(index, content){  
+        $('.collapsed').each(function (index, content) {
             // Gets the each text/content of the .collapsed class            
-            var content = $(content).text();   
+            var content = $(content).text();
             // “i” is used to ignoring cases and “g” is used to search patterns throughout the string           
-            var searchExp = new RegExp(searchText, "ig");  
+            var searchExp = new RegExp(searchText, "ig");
             // Check if the searchText is present in the content
             var matches = content.match(searchExp);
             // If matches is not null then highlight the searchText
-            if(matches) {
+            if (matches) {
                 // Loop through the matches
-                $(this).html(content.replace(searchExp, function(match){
-                    return "<span class='highlight'>" + match + "</span>";
+                $(this).html(content.replace(searchExp, function (match) {
+                    return "<mark>" + match + "</mark>";
                 }));
-            }               
-        }) 
+            }
+        })
     }
 })
 

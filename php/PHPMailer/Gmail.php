5bd07e322fb67dd3f70991c6f2a08942321f8d54
Marc John <castillomarcjohn@gmail.com>
2022-06-23 10:16:04 +0800

    Added a app password for phpmailer account

--- php/PHPMailer/Gmail.php
+++ php/PHPMailer/Gmail.php
@@ -1,5 +1,5 @@
 <?php
     //define Gmail account host for PHPMailer
     define('EMAIL', 'techploymentPH@gmail.com');
-    define('PASSWORD', 'techph2022');
+    define('PASSWORD', 'sgrgarriohthonxj');
 ?>

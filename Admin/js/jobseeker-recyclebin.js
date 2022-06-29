b1cd3365f03314c799fefd9fb69542cdde80fcfc
Roi Guiao <realrobert66@gmail.com>
2022-06-29 11:08:05 +0800

    test

--- Admin/js/jobseeker-recyclebin.js
+++ Admin/js/jobseeker-recyclebin.js
@@ -110,7 +110,7 @@ $(document).ready(function () {
             },
             success: function (data) {
                 $('#modal-delete').modal('hide');
-                toastr.success('', 'Successfully Deleted!');
+                
                 load_data(GetSearchValue(), getCurrentPage());
             }
         })

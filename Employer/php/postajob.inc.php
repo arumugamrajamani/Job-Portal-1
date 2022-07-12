a6a71f495a0f401a234c526a6da7a87f431fd5da
ptatasu <garymason922@gmail.com>
2022-07-12 16:00:14 +0800

    data also showing in job management

--- Employer/php/postajob.inc.php
+++ Employer/php/postajob.inc.php
@@ -53,14 +53,14 @@
             $companyName = mysqli_real_escape_string($conn, $companyName);
             $jobTitle = mysqli_real_escape_string($conn, $jobTitle);
             $employment = mysqli_real_escape_string($conn, $employment);
-            $jobCatergory = mysqli_real_escape_string($conn, $jobCatergory);
+            $jobCategory = mysqli_real_escape_string($conn, $jobCategory);
             $jobDescription = mysqli_real_escape_string($conn, $jobDescription);
             $salaryWage = mysqli_real_escape_string($conn, $salaryWage);
             $employerEmail = mysqli_real_escape_string($conn, $employerEmail);
             $primarySkill = mysqli_real_escape_string($conn, $primarySkill);
             $secondarySkill = mysqli_real_escape_string($conn, $secondarySkill);
             $uid = mysqli_real_escape_string($conn, $_SESSION['user_id']);
-            mysqli_query($conn,"INSERT INTO `jobpost`(`company_name`, `job_title`, `employment_type`, `job_category`, `job_description`, `salary`, `employer_email`, `primary_skill`, `secondary_skill`, `postedby_uid`, `date_posted`) VALUES ('$companyName', '$jobTitle', '$employment', '$jobCatergory', '$jobDescription', '$salaryWage', '$employerEmail','$primarySkill', '$secondarySkill','$uid',NOW())");
+            mysqli_query($conn,"INSERT INTO `jobpost`(`company_name`, `job_title`, `employment_type`, `job_category`, `job_description`, `salary`, `employer_email`, `primary_skill`, `secondary_skill`, `postedby_uid`, `date_posted`) VALUES ('$companyName', '$jobTitle', '$employment', '$jobCategory', '$jobDescription', '$salaryWage', '$employerEmail','$primarySkill', '$secondarySkill','$uid',NOW())");
 
             $response = array('status' => 'success');
         }

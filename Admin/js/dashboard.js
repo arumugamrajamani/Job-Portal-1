865b6fe26651b8af3305eea7087243ffcc975c35
ptatasu <garymason@gmail.com>
2022-07-19 08:41:56 +0800

    fixed indentation

--- Admin/js/dashboard.js
+++ Admin/js/dashboard.js
@@ -9,48 +9,45 @@ function closeNav() {
 }
 
  $.ajax({
-            url: "php/dashboard.inc.php",
-            type: "POST",
-            data:{
-                getCategory: true
+        url: "php/dashboard.inc.php",
+        type: "POST",
+        data:{
+            getCategory: true
+        },
+        dataType: "JSON",
+        success: function (data) {
+            new Array(values = []);
+            values.push(data.virtual);
+            values.push(data.web);
+            values.push(data.graphic);
+            values.push(data.projMnmt);
+            values.push(data.count);
+            values.push(data.low);
+
+             // Code for the dashboard charts
+            var xValues = ["Virtual Assistant", "Web Development", "Graphic and Multimedia", "Project Management"];
+            var barColors = ["#50677B", "#372732", "#000000", " #EDBEA4",];
+            new Chart("myChart", {
+            type: "bar",
+            data: {
+                labels: xValues,
+                datasets: [{
+                    backgroundColor: barColors,
+                    data: values
+                }]
             },
-            dataType: "JSON",
-            success: function (data) {
-                //var data = JSON.parse(response);
-                new Array(webDev = []);
-                webDev.push(data.virtual);
-                webDev.push(data.web);
-                webDev.push(data.graphic);
-                webDev.push(data.projMnmt);
-                webDev.push(data.count);
-                webDev.push(data.low);
-                $('#test').html(webDev);
-//alert(webDev);
-
-// Code for the dashboard charts
-var xValues = ["Virtual Assistant", "Web Development", "Graphic and Multimedia", "Project Management"];
-//var yValues = [30, 20, 27, 13, 0,40];
-var barColors = ["#50677B", "#372732", "#000000", " #EDBEA4",];
-
-new Chart("myChart", {
-    type: "bar",
-    data: {
-        labels: xValues,
-        datasets: [{
-            backgroundColor: barColors,
-            data: webDev
-        }]
-    },
-    options: {
-        legend: { display: false },
-        title: {
-            display: true,
-            text: "Job Categories Number"
-        }
-    }
-});
+            options: {
+                legend: {
+                    display: false
+                },
+                title: {
+                    display: true,
+                    text: "Job Categories Number"
+                }
             }
         });
+    }
+ });
 
 
 var xValues = ["ACTIVE", "INACTIVE",];

2c6c47236347fde4d9f64786993e2f4229857ac4
ptatasu <garymason922@gmail.com>
2022-07-20 14:04:14 +0800

    Edit Profile Working
    added sweet alert

--- Jobseeker/js/manage-account-1.js
+++ Jobseeker/js/manage-account-1.js
@@ -24,18 +24,56 @@ $(document).ready(function () {
             },
             dataType: "JSON",
             success: function (data) {
-                console.log(data);
-                swal({
-                    title: "Profile Updated Successfully",
-                    icon: "success",
-                    button: "Okay",
-                })
-                .then(function() {
-                    window.location = "applicant-profile.php";
-                });
+                if (data.status == 'success') {
+                    swal({
+                        title: "Profile Updated Successfully",
+                        icon: "success",
+                        button: "Okay",
+                    })
+                    .then(function() {
+                            window.location = "applicant-profile.php";
+                    });
+                }
+                else if (data.status == 'error'){
+                    if(data.fullname.status == 'error') {
+                        $('#fullname').removeClass().addClass('form-control border-danger');
+                        $('#fullname').popover({ placement: 'right', content: data.fullname.message }).popover('show');
+                    } 
+                    else {
+                        $('#fullname').removeClass().addClass('form-control border-success').popover('dispose');
+                    }
+                    if(data.birthday.status == 'error') {
+                        $('#birthday').removeClass().addClass('form-control border-danger');
+                        $('#birthday').popover({ placement: 'right', content: data.birthday.message }).popover('show');
+                    } 
+                    else {
+                        $('#birthday').removeClass().addClass('form-control border-success').popover('dispose');
+                    }
+                    if(data.address.status == 'error') {
+                        $('#address').removeClass().addClass('form-control border-danger');
+                        $('#address').popover({ placement: 'right', content: data.address.message }).popover('show');
+                    } 
+                    else {
+                        $('#address').removeClass().addClass('form-control border-success').popover('dispose');
+                    }
+                    if(data.email.status == 'error') {
+                        $('#email').removeClass().addClass('form-control border-danger');
+                        $('#email').popover({ placement: 'right', content: data.email.message }).popover('show');
+                    } 
+                    else {
+                        $('#email').removeClass().addClass('form-control border-success').popover('dispose');
+                    }if(data.mobile.status == 'error') {
+                        $('#mobile_number').removeClass().addClass('form-control border-danger');
+                        $('#mobile_number').popover({ placement: 'right', content: data.mobile.message }).popover('show');
+                    } 
+                    else {
+                        $('#mobile_number').removeClass().addClass('form-control border-success').popover('dispose');
+                    }
+                }
             }
         });
     });
+
     function fetchData() {
         $.ajax({
             url: 'php/manage-account-1.inc.php',
--- Jobseeker/php/manage-account-1.inc.php
+++ Jobseeker/php/manage-account-1.inc.php
@@ -3,6 +3,24 @@
 include '../../php/db-connection.php';
 session_start();
 
+// Function for validating if input is valid fullname
+function isValidFullname($fullname) {
+    if (preg_match("/^[a-zA-Z .]*$/", $fullname)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+// Function for validating if inout is valid number
+function isValidNumber($number)
+{
+    if (preg_match("/^[0-9]*$/", $number)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
 // When the page is loaded the js will call for this then this will get the admin's data from the DB
 if (isset($_POST['fetchData'])) {
     $jobseekerId = $_SESSION['user_id'];
@@ -30,7 +48,61 @@ if (isset($_POST['updateData'])){
     $address = $_POST['address'];
     $email = $_POST['email'];
     $mobileNumber = $_POST['mobileNumber'];
-    $fetchDetailsQuery = mysqli_query($conn, "UPDATE `jobseeker` SET `fullname`='$fullname',`mobile_number`='$mobileNumber',`email`='$email' WHERE jobseeker_id = $jobseekerId");
-    //$row = mysqli_fetch_assoc($fetchDetailsQuery);
-    echo json_encode($_POST);
+    //Validation for fullname
+    if (empty($fullname)) {
+        $fullnameInfo = array('status' => 'error', 'message' => 'Fullname is required.');
+    }
+    elseif (!isValidFullname($fullname)) {
+        $fullnameInfo = array('status' => 'error', 'message' => 'Only characters are allowed.');
+    }
+    else {
+        $fullnameInfo = array('status' => 'success');
+    }
+    // Validation for address
+    if (empty($address)) {
+        $addressInfo = array('status' => 'error', 'message' => 'Address is required.');
+    }
+    else {
+        $addressInfo = array('status' => 'success');
+    }
+
+    if (empty($birthday)) {
+        $birthdayInfo = array('status' => 'error', 'message' => 'Date is required.');
+    }
+    else {
+        $birthdayInfo = array('status' => 'success');
+    }
+    // Validation for email 
+    if (empty($email)) {
+        $emailInfo = array('status' => 'error', 'message' => 'Email Address is required.');
+    }
+    elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $emailInfo = array('status' => 'error', 'message' => 'Email is invalid.');
+    }
+    else {
+        $emailInfo = array('status' => 'success');
+    }
+    // Validation for company size
+     if (empty($mobileNumber)) {
+         $mobileNumberInfo = array('status' => 'error', 'message' => 'Mobile Number is required.');
+        } elseif (!isValidNumber($mobileNumber)) {
+            $mobileNumberInfo = array('status' => 'error', 'message' => 'Only numbers are allowed.');
+    } else {
+        $mobileNumberInfo = array('status' => 'success');
+    }
+    
+    if ($fullnameInfo['status'] == 'error' || empty($birthday) || empty($address) || $emailInfo['status'] == 'error' || $mobileNumberInfo['status'] == 'error'){
+        $response = array('status' => 'error', 'fullname' => $fullnameInfo, 'birthday' => $birthdayInfo, 'address' => $addressInfo, 'email' => $emailInfo, 'mobile' => $mobileNumberInfo);
+    }
+    else{ 
+        $jobseekerId = $_SESSION['user_id'];
+        $fullname = mysqli_real_escape_string($conn, $_POST['fullname']);
+        $birthday = mysqli_real_escape_string($conn, $_POST['birthday']);
+        $address = mysqli_real_escape_string($conn, $_POST['address']);
+        $email = mysqli_real_escape_string($conn, $_POST['email']);
+        $mobileNumber = mysqli_real_escape_string($conn, $_POST['mobileNumber']);
+        $fetchDetailsQuery = mysqli_query($conn, "UPDATE `jobseeker` SET `fullname`='$fullname',`mobile_number`='$mobileNumber',`email`='$email' WHERE jobseeker_id = $jobseekerId");
+        $response = array('status' => 'success');
+    }
+    echo json_encode($response);
 }

2e4123b4ca58a0d279c20a4b32309bccd3f4fa8d
ptatasu <garymason922@gmail.com>
2022-07-21 10:16:22 +0800

    added js and inc file

---
+++ Jobseeker/php/jobapplication.inc.php
@@ -0,0 +1,38 @@
+<?php
+//includes db connection from 2 folders back
+include '../../php/db-connection.php';
+session_start();
+
+// check if profile pic is not null && if file exists  then returns a string value of the profile picture location
+function  getProfilePicLoc($profilePic)
+{
+    if ($profilePic != NULL && file_exists("../../storage/" . $profilePic)) {
+        return "../storage/" . $profilePic;
+    } else {
+        return "../storage/noProfilePic.png";
+    }
+}
+
+// When the page is loaded the js will call for this then this will get the admin's data from the DB
+if (isset($_POST['getData'])) {
+    $jobseekerId = $_SESSION['user_id'];
+    // Create query to get the employer information
+    $fetchDetailsQuery = mysqli_query($conn, "SELECT * FROM jobseeker WHERE jobseeker_id = '$jobseekerId'");
+    $row = mysqli_fetch_assoc($fetchDetailsQuery);
+    // Get the employer information needed to edit modal
+    $profilePic = GetProfilePicLoc($row['profile_picture']);
+    $fullName = $row['fullname'];
+    $email = $row['email'];
+    $number = $row['mobile_number'];
+
+    // Create Assoc array to return to the ajax call
+    $response = array(
+        'profile_picture' => $profilePic,
+        'fullname' => $fullName,
+        'email' => $email,
+        'mobile_number' => $number
+
+        
+    );
+    echo json_encode($response);
+}

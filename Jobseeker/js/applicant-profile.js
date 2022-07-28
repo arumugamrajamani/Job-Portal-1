37a98078201ee025fe6b215e266ff7d199cec203
ptatasu <garymason922@gmail.com>
2022-07-28 13:36:41 +0800

    job seeker profile showing details

--- Jobseeker/js/applicant-profile.js
+++ Jobseeker/js/applicant-profile.js
@@ -13,12 +13,16 @@ $(document).ready(function () {
             },
             dataType: "JSON",
             success: function (data) {
+                //alert(data.address);
+                //dropdown name
+                $('#upperName').html(data.upperName);
                 //assign got value to the html ids
                 $('#profile_picture').attr('src', data.profile_picture);
                 $('#fullname').html(data.fullname);
                 $('#mobile_number').html("Number: "+data.mobile_number);
                 $('#email').html("Email: "+data.email);
-                $('#upperName').html(data.upperName);
+                $('#experience').html(data.experience);
+                $('#address').html("<i class='bi bi-geo-alt'></i>"+data.address);
             }
         });
     }

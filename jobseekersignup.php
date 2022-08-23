cb760d7feee147b77ec37463198c2e6975f47713
Markobry <Markobrero14@gmail.com>
2022-08-23 16:09:34 +0800

    jobseeker signup design

--- jobseekersignup.php
+++ jobseekersignup.php
@@ -24,15 +24,15 @@
 <body>
     <div class="color-overlay">
         <div class="container-fluid">
-            <nav id="navbar-example2" class="navbar navbar-expand-lg navbar-light shadow" >
+            <nav id="navbar-example2" class="navbar navbar-expand-lg h6 navbar-light bg fixed-top mx-0 shadow " >
                     <a href="#" class="navbar-brand ms-3">
-                    <img class="logo" src="image/flogo.png" alt="Job Portal Logo" width="120" height="95"></a>
+                    <img class="logo" src="image/light-logo.png" alt="Job Portal Logo" width="120" height="95" id="logo"></a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#toggleMobileMenu" aria-controls="toggleMobileMenu" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
                     <div class="collapse navbar-collapse" id="toggleMobileMenu">
                     <ul class="navbar-nav ms-auto text-center fw-bold">
-                            <nav class="navbar navbar-light">     
+                            <nav class="navbar navbar-light hatd">     
                                 <li ><a class="nav-link me-5 " href="index.php#home">Home</a></li>
                                 <li ><a class="nav-link me-5 " href="index.php#aboutus">About Us</a></li>
                                 <li><a class="nav-link me-5 " href="index.php#contactus">Contact Us</a></li>
@@ -51,27 +51,34 @@
             </nav>         
         </div>
     </div>
+    <div class="swits">
+        <div class = 'toggle-switch'>
+            <label class="lab">
+              <input class="dar" type = 'checkbox' onclick="toggleImage()">
+              <span id="icon2" class = 'slider'></span>
+            </label>
+          </div>
+        </div>
 
-    
     <div class="masthead">
-        <div class="ms-5 mb-5 div1"><br><br>
+        <div class="div1"><br><br>
             <form class="container needs-validation" novalidate>
                 <div class="col-sm-9 text-start row mb-3 div2">
-                    <h1 class="text-dark fw-bold">JOB SEEKER REGISTRATION FORM</h1>
+                    <h1 class="fw-bold">JOB SEEKER REGISTRATION FORM</h1>
                 </div>
-                <h2 class="text-black text-center mt-4">ACCOUNT INFORMATION</h2>
+                <h2 class="text-center mt-4">ACCOUNT INFORMATION</h2>
 				<div class="row mb-3 mt-3 ms-4">
                     <label for="Profile" class="profile-picture col-sm-2">Profile Picture:</label>
                     <div class="col-sm-7">
                         <input type="file" id="profilePic" class="file form-control" name="Profile">
-                        <div class="text-danger"> Please attach a file</div>
+                        <div class="text-danger"></div>
                     </div>
                 </div>
 				<div class="row mb-3 mt-3 ms-4">
                     <label for="Resume" class="profile-picture col-sm-2">Resume:</label>
                     <div class="col-sm-7">
                         <input type="file" id="resume" class="file form-control" name="Resume">
-                        <div class="text-danger"> Please attach a file</div>
+                        <div class="text-danger"></div>
                     </div>
                 </div>
                  <div class="row mb-3 mt-3 ms-4">
@@ -79,71 +86,71 @@
                     <label for="fullname" class="col-sm-2 col-form-label">Full Name</label>
 
                     <div class="col-sm-9">
-                        <input type="text" class="form-control error" id="fullname">               
-                        <div class="text-danger"> Please enter full name</div>
+                        <input type="text" class="form-control" id="fullname">               
+                        <div class="text-danger"></div>
                     </div>
                 </div>
                 <div class="row mb-3 ms-4">
                     <label for="email" class="col-sm-2 col-form-label">Email</label>
                     <div class="col-sm-9">
-                        <input type="email" class="form-control error" id="email">
-                        <div class="text-danger"> Please enter your email address</div>
+                        <input type="email" class="form-control" id="email">
+                        <div class="text-danger"></div>
                     </div>
                 </div>
                 <div class="row mb-3 mt-3 ms-4">
                     <label for="fullname" class="col-sm-2 col-form-label">Address</label>
                     <div class="col-sm-9">
-                        <input type="text" class="form-control error" id="address">               
-                        <div class="text-danger"> Please enter full address</div>
+                        <input type="text" class="form-control" id="address">               
+                        <div class="text-danger"></div>
                     </div>
                 </div>
                 <div class="row mb-3 mt-3 ms-4">
                     <label for="fullname" class="col-sm-2 col-form-label">Birthday</label>
                     <div class="col-sm-9">
-                        <input type="text" class="form-control error" id="birthday">               
-                        <div class="text-danger"> Please enter your birthday</div>
+                        <input type="text" class="form-control" id="birthday">               
+                        <div class="text-danger"></div>
                     </div>
                 </div>
                 <div class="row mb-3 ms-4">
                     <label for="password" class="col-sm-2 col-form-label">Password</label>
                     <div class="col-sm-9">
                         <input type="password" required="" class="form-control" id="password">
-                        <div class="invalid-feedback"> Please enter your password</div>
+                        <div class="invalid-feedback"></div>
                     </div>
                 </div>
                 <div class="row mb-3 ms-4">
                     <label for="confirmpassword" class="col-sm-2 col-form-label">Confirm Password</label>
                     <div class="col-sm-9">
                         <input type="password" required="" class="form-control" id="confirmpassword">
-                        <div class="invalid-feedback"> Please confirm your password</div>
+                        <div class="invalid-feedback"></div>
                     </div>
                 </div>
                 <div class="row mb-3 ms-4">
                     <label for="mobilenumber" class="col-sm-2 col-form-label">Mobile Number</label>
                     <div class="col-sm-9">
                         <input type="number" required="" class="form-control" id="mobilenumber">
-                        <div class="invalid-feedback"> Please enter your mobile number</div>
+                        <div class="invalid-feedback"></div>
                     </div>
                 </div>
                 <div class="row mb-3 ms-4">
                     <label for="mobilenumber" class="col-sm-2 col-form-label">Expected Salary</label>
                     <div class="col-sm-9">
                         <input type="number" required="" class="form-control" id="salary">
-                        <div class="invalid-feedback"> Please enter your desire amount salary</div>
+                        <div class="invalid-feedback"></div>
                     </div>
                 </div>
                 <div class="row mb-3 mt-3 ms-4">
                     <label for="fullname" class="col-sm-2 col-form-label">H.E. Attainment</label>
                     <div class="col-sm-9">
-                        <input type="text" class="form-control error" id="attainment">               
-                        <div class="text-danger"> Type in your educational background</div>
+                        <input type="text" class="form-control" id="attainment">               
+                        <div class="text-danger"></div>
                     </div>
                 </div>
                 <div class="row mb-3 ms-4">
                     <label for="mobilenumber" class="col-sm-2 col-form-label">Available Hours</label>
                     <div class="col-sm-9">
                         <input type="number" required="" class="form-control" id="hours">
-                        <div class="invalid-feedback"> Please enter your desire amount in hours</div>
+                        <div class="invalid-feedback"></div>
                     </div>
                 </div>
                 <div class="row mb-3 mt-3 ms-4">
@@ -190,10 +197,52 @@
                 </div>
             </form>
             <div class="online-reg">
-                <img class="online" src="image/online-registration.png" alt="Registration">
+                <img class="online" src="image/online-registration 1.png" alt="Registration">
             </div>
         </div>
     </div>
+    <script>(function () {
+        'use strict'
+        // Fetch all the forms we want to apply custom Bootstrap validation styles to
+        var forms = document.querySelectorAll('.needs-validation')
+        // Loop over them and prevent submission
+        Array.prototype.slice.call(forms)
+        .forEach(function (form) {
+            form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+        }
+        form.classList.add('was-validated')
+            }, false)
+        })
+        })()
+    </script>
+    <script>
+        var icon2 = document.getElementById("icon2")
+    
+        icon2.onclick = function(){
+          document.body.classList.toggle("dark-theme");
+        }
+        
+        function toggleImage(){
+          imgsrc= document.getElementById("logo").src;
+        if (imgsrc.indexOf("image/light-logo.png") !=-1){
+          document.getElementById("logo").src = "image/Techployment (7) 1.png";
+        }
+        else{
+          document.getElementById("logo").src = "image/light-logo.png";
+        }
+        imgsrc= document.getElementById("Selection").src;
+        if (imgsrc.indexOf("/Landing-Page/image/icons8-menu-60.png") !=-1){
+          document.getElementById("Selection").src = "/Landing-Page/image/selection.png";
+        }
+        else{
+          document.getElementById("Selection").src = "/Landing-Page/image/icons8-menu-60.png";
+        }
+        }
+      </script>
+
 
     <script src="js/jobseekersignup.js"></script>
     <script>(function () {
@@ -213,5 +262,6 @@
         })
         })()
     </script>
+
 </body>
 </html>

2e89e2c417457d1e8f89f2395083de6c3284fc44
MayorViel <109166861+MayorViel@users.noreply.github.com>
2022-09-15 16:34:48 +0800

    Add files via upload
    Navigation Bar Fixed, and Some Functionalities on optimization of front end of InsideJob-home

--- InsideJob-home.php
+++ InsideJob-home.php
@@ -1,178 +1,135 @@
-
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"crossorigin="anonymous"></script>
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
-  <!-- jQuery cdn link below -->
-  <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
-  <title>Inside the Job</title>
-  <link rel="stylesheet" href="css/InsideJob.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Techployment</title>
+    <!--Font-->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,455;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Raleway:wght@300&display=swap" rel="stylesheet">
+    <!--JS CDN-->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <!--Bootstrap-->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <!-- JQuery CDN below -->
+    <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+    <!-- Toast CDN for functionality of toastr -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.css" integrity="sha512-3pIirOrwegjM6erE5gPSwkUzO+3cTjpnV9lexlNZqvupR64iZBnOOTiiLPb9M36zpMScbmUNIcHUqKD47M719g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- Toast CDN for design of toastr -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js" integrity="sha512-VEd+nq25CkR676O+pLBnDW09R7VQX9Mdiij052gVCp5yVH3jGtH70Ho/UUv4mJDsEdTvqRCFZg0NKGiojGnUCw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <link rel="stylesheet" href="css/index.css">
 </head>
+
 <body data-bs-spy="scroll" data-bs-target="#navbar-example2">
-  <!-- <div class="color-overlay">
-          <div class="container-fluid">
-              <nav id="navbar-example2" class="navbar navbar-expand-lg navbar-light fixed-top mx-0 shadow " >
-                  <a href="#" class="navbar-brand ms-3">
-                  <img src="image/light-logo.png" alt="Job Portal Logo" width="100" height="80"></a>
-              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#toggleMobileMenu" aria-controls="toggleMobileMenu" aria-expanded="false" aria-label="Toggle navigation">
-                  <span class="navbar-toggler-icon"></span>
-              </button>
-              <div class="collapse navbar-collapse" id="toggleMobileMenu"> 
-                      <ul class="navbar-nav ms-auto fw-bold mb-lg-0">    
-                          <li><a class="nav-link" href="index.php#home">Home</a></li>
-                          <li><a class="nav-link" href="index.php#aboutus">About Us</a></li>
-                          <li><a class="nav-link" href="index.php#contactus">Contact Us</a></li>
-                          <li><a class="nav-link" href="index.php#faq">FAQ</a></li>
-                          <li class="nav-item dropdown">
-                          <a class="nav-link dropdown-toggle  mx-4" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">Signup</a>
-                          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                              <li><a class="dropdown-item " href="companyregister.php">Employer</a></li>
-                              <li><a class="dropdown-item " href="jobseekersignup.php">JobSeeker</a></li>
-                          </ul>
-                          <li><a class="nav-link" href="login.php">Login</a></li>
-                      </ul>
-                  </div>
-              </nav>         
-          </div>
-      </div> -->
-      <nav class="navbar navbar-expand-lg navbar-light bg-light shadow p-0" id="nav0">
-        <div class="container-fluid" id="inner">
-          <a class="navbar-brand me-1" href="#"></a>
-          <img class="img" src="image/Techployment (7) 1.png" onclick="window.location.href='index.php#home'" alt="Job Portal Logo" width="95" height="90" id="logo"></a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <img src="image/selection.png" alt="" width="40px" height="50px" id="Selection">
-          </button>
-          <div class="collapse navbar-collapse" id="toggleMobileMenu"> 
-                      <ul class="navbar-nav ms-auto fw-bold mb-lg-0">    
-                          <li><a class="nav-link" href="index.php#home">Home</a></li>
-                          <li><a class="nav-link" href="index.php#aboutus">About Us</a></li>
-                          <li><a class="nav-link" href="index.php#contactus">Contact Us</a></li>
-                          <li><a class="nav-link" href="index.php#faq">FAQ</a></li>
-                          <li class="nav-item dropdown">
-                          <a class="nav-link dropdown-toggle  mx-4" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">Signup</a>
-                          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                              <li><a class="dropdown-item " href="companyregister.php">Employer</a></li>
-                              <li><a class="dropdown-item " href="jobseekersignup.php">JobSeeker</a></li>
-                          </ul>
-                          <li><a class="nav-link" href="login.php">Login</a></li>
-                      </ul>
-                  </div>
-        </div>
+    <div class="color-overlay">
+        <?php include_once 'include/navbar.php'; ?>
     </div>
-    </nav>
-    <div class="swits">
-    <div class = 'toggle-switch'>
-      <label class="lab">
-        <input class="dar" type = 'checkbox' onclick="toggleImage()">
-        <span id="icon2" class = 'slider'></span>
-      </label>
+    
+    <div class="color-overlay">
+        <?php include_once 'include/Inside Job.php'; ?>
     </div>
-    </div>
-    <div class="container shadow-lg" id="content">
-      <div class="container-fluid" id="ITS">
-        <div class="support">
-          <h4><b id="jobTitle"></h4></b>
-          <p id="companyName"> </p>
-          <p id="address"></p>
-        </div>
-      </div>
-      <div class="container-fluid" id="sal">
-        <div class="salary">
-          <h4><b id="salaryy"></h4></b>
-          <h5>JOB TYPE</h5>
-        <p id="employment"></p>
-        </div>  
-      </div>
-      <div class="container-fluid" id="job">
-        <div class="job desc">
-          <h5><b>JOB DESCRIPTION</b> (Includes skill requirements/text type)</h5>
-          <p><u id="description"></u></p>
-        </div>
-      </div>
-      <div class="container-fluid" id="hire">
-        <div class="hiring">
-          <h4><b>HIRING INSIGHTS</h4></b>
-          <p>Hiring 1 candidate for this role</p>
-        </div>
-      </div>
-      <div class="container-fluid" id="act">
-        <div class="activity">
-          <h4><b>JOB ACTIVITY</b></h4>
-          <p>Employer reviewed job/date</p>
-          <h5>Date Posted</h5>
-          <h6><b id="datePosted"></b></h6>
-        </div>
-      </div>
-    </div>
-    <div class="container shadow-lg" id="content1">
-      <div class="container-fluid" id="app">
-        <div class="apply now">
-          <h3><b id="jobTitle1"></b></h3>
-          <p id="companyName1"></p>
-         <p id="address1"></p>
-        </div>
-      </div><br>
-      <div class="container-fluid" id="sal1">
-        <div class="salary1">
-          <h4><b id="salaryy1"></b></h4>
-          <p>You must qualified to this position</p>
-          <div class="butt">
-            <button id="applyJob" class="logapp appnow" onclick="window.location.href='login.php'" style="text-decoration: none;">APPLY NOW</button>
-          </div>
-        </div>
-      </div>
-    </div><!-- <a class="appnow" onclick="apply()" style="text-decoration: none;">APPLY NOW</a> -->
-    <div class="container shadow-lg" id="content2">
-      <div class="container-fluid" id="req">
-        <div class="requirements">
-          <ul id="bullet"></ul>
-          <h4><b>SKILL REQUIREMENTS</b></h4><br>
-          <p>• Expert knowledge in operating systems<br>
-          • Expert knowledge in working system<br>
-          • Ability to prioritize your workload<br>
-          • Capacity to clearly explain a technical problem</p>
-        </div>
-      </div>
-    </div>  
 
     
+
+
+    <footer class="page-footer shadow-sm"><br>
+        <div class="container-fluid text-start text-md-left mt-5 px-4">
+            <div class="row">
+                <div class="col-md-2 col-lg-2 g-0  mb-4">
+                    <h6 class="text-uppercase ">JOB SEEKER</h6>
+                    <ul class="list-unstyled">
+                        <li class="my-0"><a href="jobseekersignup.php" class="text-dark">Register</a></li>
+                        <li class="my-0"><a href="searchjob-home.php" class="text-dark">Job Search</a></li>
+                        <li class="my-0"><a href="howitworks-jobseeker.php" class="text-dark">How it works</a></li>
+                    </ul>
+                </div>
+                <div class="col-md-2 col-lg-2 g-0  mb-4">
+                    <h6 class="text-uppercase ">EMPLOYER</h6>
+                    <ul class="list-unstyled">
+                        <li class="my-0"><a href="companyregister.php" class="text-dark">Register</a></li>
+                        <li class="my-0"><a href="../Job-Portal/Employer/postajob.php" class="text-dark">Post a job</a></li>
+                        <li class="my-0"><a href="howitworks-employ.php" class="text-dark">How it works</a></li>
+
+                    </ul>
+                </div>
+                <div class="col-md-2 col-lg-2 g-0  mb-4 me-5">
+                    <h6 class="text-uppercase ">OTHER INFORMATIONS</h6>
+
+                    <ul class="list-unstyled">
+                        <li class="my-0"></i> <a href="index.php#faq" class="text-dark">FAQ</a></li>
+                        <li class="my-0"></i> <a href="index.php#aboutus" class="text-dark">About us</a></li>
+                        <li class="my-0"></i><a href="privacypolicy.php" class="text-dark">Privacy Policy</a></li>
+                        <li class="my-0"></i> <a href="termsofuse.php" class="text-dark">Terms of use</a></li>
+                        <li class="my-0"></i> <a href="termsandcondition.php" class="text-dark">Terms and conditions</a></li>
+                    </ul>
+                </div>
+                <div class="col-md-2 col-lg-2 g-0  mb-4">
+                    <h6 class="text-uppercase ">Credits</h6>
+                    <ul class="list-unstyled">
+                        <li class="my-0"><a href="developer-team.php" class="text-dark">Developer</a></li>
+                        <li class="my-0"><a href="credit-iconscout.php" class="text-dark">Iconscout</a></li>
+                        <li class="my-0"><a href="credit-flaticon.php" class="text-dark">Flaticon</a></li>
+                        <li class="my-0"><a href="credit-otherinfo.php" class="text-dark">Web icons</a></li>
+
+                    </ul>
+                </div>
+                <div class="col-md-2 col-lg-3 g-0 mb-4 text-center contact-us">
+                    <h6 class="text-uppercase ">CONTACT US</h6>
+
+                    <ul class="list-unstyled d-flex justify-content-center con-img">
+                        <img class="my-0" src="image/gmail.png" alt="logo" href="#" style="width: 50px; height: 25px">
+                        concerns.techploymentph@gmail.com
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <div class="container-fluid d-flex bg-white pt-3">
+        <div class="d-flex justify-content-start">
+            <h5> Copyrignt © TechPloyment 2022. All Rights Reserved.</h5>
+        </div>
+        <div class="ms-auto">
+            <h5>Developed by MCC Interns 2022, Job Portal</h5>
+        </div>
+        <br>
+    </div>
+
     <script>
-      <?php 
-        $postId = isset($_GET['postId']) ? $_GET['postId'] : '';  
-      ?>
-
-      var postId = "<?= $postId ?>";
-
-      var icon2 = document.getElementById("icon2")
-  
-      icon2.onclick = function() {
-        document.body.classList.toggle("dark-theme");
-      }
-      
-      function toggleImage(){
-        imgsrc= document.getElementById("logo").src;
-        if (imgsrc.indexOf("image/light-logo.png") !=-1){
-          document.getElementById("logo").src = "image/Techployment (7) 1.png";
-        }
-        else{
-          document.getElementById("logo").src = "image/light-logo.png";
-        }
-        imgsrc= document.getElementById("Selection").src;
-        if (imgsrc.indexOf("image/selection.png") !=-1){
-          document.getElementById("Selection").src = "image/icons8-menu-60.png";
-        }
-        else{
-          document.getElementById("Selection").src = "image/selection.png";
-        }
-      }
+        var scrollSpy = new bootstrap.ScrollSpy(document.body, {
+            target: '#navbar-example2'
+        })
     </script>
-    <script src="js/insidejob.js"></script>
-    <script src="js/pfp.js"></script>
+
+    <script>
+        var icon2 = document.getElementById("icon2")
+
+        icon2.onclick = function() {
+            document.body.classList.toggle("dark-theme");
+        }
+
+        function toggleImage() {
+            imgsrc = document.getElementById("logos").src;
+            if (imgsrc.indexOf("image/light-logo.png") != -1) {
+                document.getElementById("logos").src = "image/Techployment (7) 1.png";
+            } else {
+                document.getElementById("logos").src = "image/light-logo.png";
+            }
+            imgsrc = document.getElementById("men").src;
+            if (imgsrc.indexOf("/Landing-Page/image/icons8-menu-60.png") != -1) {
+                document.getElementById("men").src = "/Landing-Page/image/selection.png";
+            } else {
+                document.getElementById("men").src = "/Landing-Page/image/icons8-menu-60.png";
+            }
+        }
+    </script>
+
+    <script src="js/faq.js"></script>
+    <script src="js/contactus.js"></script>
+    <script src="js/faqsearch.js"></script>
+    <script src="js/system-info.js"></script>
 </body>
+
 </html>
---
+++ include/Inside Job.php
@@ -0,0 +1,182 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
+  <!-- jQuery cdn link below -->
+  <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+  <title>Inside the Job</title>
+  <link rel="stylesheet" href="css/InsideJob.css">
+</head>
+<body data-bs-spy="scroll" data-bs-target="#navbar-example2">
+  <!-- <div class="color-overlay">
+          <div class="container-fluid">
+              <nav id="navbar-example2" class="navbar navbar-expand-lg navbar-light fixed-top mx-0 shadow " >
+                  <a href="#" class="navbar-brand ms-3">
+                  <img src="image/light-logo.png" alt="Job Portal Logo" width="100" height="80"></a>
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#toggleMobileMenu" aria-controls="toggleMobileMenu" aria-expanded="false" aria-label="Toggle navigation">
+                  <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="toggleMobileMenu"> 
+                      <ul class="navbar-nav ms-auto fw-bold mb-lg-0">    
+                          <li><a class="nav-link" href="index.php#home">Home</a></li>
+                          <li><a class="nav-link" href="index.php#aboutus">About Us</a></li>
+                          <li><a class="nav-link" href="index.php#contactus">Contact Us</a></li>
+                          <li><a class="nav-link" href="index.php#faq">FAQ</a></li>
+                          <li class="nav-item dropdown">
+                          <a class="nav-link dropdown-toggle  mx-4" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">Signup</a>
+                          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                              <li><a class="dropdown-item " href="companyregister.php">Employer</a></li>
+                              <li><a class="dropdown-item " href="jobseekersignup.php">JobSeeker</a></li>
+                          </ul>
+                          <li><a class="nav-link" href="login.php">Login</a></li>
+                      </ul>
+                  </div>
+              </nav>         
+          </div>
+      </div> -->
+      <!-- <nav class="navbar navbar-expand-lg navbar-light bg-light shadow p-0" id="nav0">
+        <div class="container-fluid" id="inner">
+          <a class="navbar-brand me-1" href="#"></a>
+          <img class="img" src="image/Techployment (7) 1.png" onclick="window.location.href='index.php#home'" alt="Job Portal Logo" width="95" height="90" id="logo"></a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <img src="image/selection.png" alt="" width="40px" height="50px" id="Selection">
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ms-auto text-center">
+                        <nav class="navbar navbar-light fw-bold">  
+                            <li><a class="nav-link me-5 fs-5" href="index.php#home">Home</a></li>
+                            <li><a class="nav-link me-5 fs-5" href="index.php#aboutus">About Us</a></li>
+                            <li><a class="nav-link me-5 fs-5" href="index.php#contactus">Contact Us</a></li>            
+                            <li><a class="nav-link me-5 fs-5" href="index.php#faq">FAQ</a></li>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle fs-5 me-4" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">Signup</a>
+                                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <li><a class="dropdown-item me-4 fs-5" href="companyregister.php">Employer</a></li>
+                                    <li><a class="dropdown-item me-4 fs-5" href="jobseekersignup.php">JobSeeker</a></li>
+                                </ul>
+                            </li>
+                            <li><a class="nav-link fs-5 ms-3 me-5" href="login.php">Login</a></li>   
+                        </nav>
+                    </ul>
+                </div>
+        </div>
+    </div>
+    </nav> -->
+
+    <div class="swits">
+    <div class = 'toggle-switch'>
+      <label class="lab">
+        <input class="dar" type = 'checkbox' onclick="toggleImage()">
+        <span id="icon2" class = 'slider'></span>
+      </label>
+    </div>
+    </div>
+    <div class="container shadow-lg" id="content">
+      <div class="container-fluid" id="ITS">
+        <div class="support">
+          <h4><b id="jobTitle"></h4></b>
+          <p id="companyName"> </p>
+          <p id="address"></p>
+        </div>
+      </div>
+      <div class="container-fluid" id="sal">
+        <div class="salary">
+          <h4><b id="salaryy"></h4></b>
+          <h5>JOB TYPE</h5>
+        <p id="employment"></p>
+        </div>  
+      </div>
+      <div class="container-fluid" id="job">
+        <div class="job desc">
+          <h5><b>JOB DESCRIPTION</b> (Includes skill requirements/text type)</h5>
+          <p><u id="description"></u></p>
+        </div>
+      </div>
+      <div class="container-fluid" id="hire">
+        <div class="hiring">
+          <h4><b>HIRING INSIGHTS</h4></b>
+          <p>Hiring 1 candidate for this role</p>
+        </div>
+      </div>
+      <div class="container-fluid" id="act">
+        <div class="activity">
+          <h4><b>JOB ACTIVITY</b></h4>
+          <p>Employer reviewed job/date</p>
+          <h5>Date Posted</h5>
+          <h6><b id="datePosted"></b></h6>
+        </div>
+      </div>
+    </div>
+    <div class="container shadow-lg" id="content1">
+      <div class="container-fluid" id="app">
+        <div class="apply now">
+          <!-- <h3><b id="jobTitle1"></b></h3> -->
+          <p id="companyName1"></p>
+         <p id="address1"></p>
+        </div>
+      </div><br>
+      <div class="container-fluid" id="sal1">
+        <div class="salary1">
+          <h4><b id="salaryy1"></b></h4>
+          <p>You must qualified to this position</p>
+          <div class="butt">
+            <button id="applyJob" class="logapp appnow" onclick="window.location.href='login.php'" style="text-decoration: none;">APPLY NOW</button>
+          </div>
+        </div>
+      </div>
+    </div><!-- <a class="appnow" onclick="apply()" style="text-decoration: none;">APPLY NOW</a> -->
+    <div class="container shadow-lg" id="content2">
+      <div class="container-fluid" id="req">
+        <div class="requirements">
+          <ul id="bullet"></ul>
+          <h4><b>SKILL REQUIREMENTS</b></h4><br>
+          <p>• Expert knowledge in operating systems<br>
+          • Expert knowledge in working system<br>
+          • Ability to prioritize your workload<br>
+          • Capacity to clearly explain a technical problem</p>
+        </div>
+      </div>
+    </div>  
+
+    
+    <script>
+      <?php 
+        $postId = isset($_GET['postId']) ? $_GET['postId'] : '';  
+      ?>
+
+      var postId = "<?= $postId ?>";
+
+      var icon2 = document.getElementById("icon2")
+  
+      icon2.onclick = function() {
+        document.body.classList.toggle("dark-theme");
+      }
+      
+      function toggleImage(){
+        imgsrc= document.getElementById("logo").src;
+        if (imgsrc.indexOf("image/light-logo.png") !=-1){
+          document.getElementById("logo").src = "image/Techployment (7) 1.png";
+        }
+        else{
+          document.getElementById("logo").src = "image/light-logo.png";
+        }
+        imgsrc= document.getElementById("Selection").src;
+        if (imgsrc.indexOf("image/selection.png") !=-1){
+          document.getElementById("Selection").src = "image/icons8-menu-60.png";
+        }
+        else{
+          document.getElementById("Selection").src = "image/selection.png";
+        }
+      }
+    </script>
+    <script src="js/insidejob.js"></script>
+    <script src="js/pfp.js"></script>
+</body>
+</html>
